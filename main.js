e5d3ed8de0c5f0f5e8e1e37d59cafd217f733fea
roman02021 <roman.zemanik@revolware.com>
2021-10-29 09:52:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const mountain = document.querySelector('.mountain');
+const trees = document.querySelector('.trees');
+const fullHora = document.querySelector('.full-hora');
+let a = 3;
+let b = 3;
+window.addEventListener('scroll', (e) => {
+    // const rect = mountain.getBoundingClientRect();
+    // console.log(window.scrollY);
+    // console.log(rect.top);
+    // a += 3;
+    // b -= 3;
+    mountain.style.transform = `translateY(${-window.scrollY / 5}px)`;
+    // trees.style.top = `${a}px`;
+});
